d8daa5724eb84772992948807254e3ca5c1cea65
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-09-28 12:26:45 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,18 @@
+console.log("34"+10)
+console.log("1"+2+3)
+console.log(1+2+"3")
+console.log(10-"34")
+console.log("10"-34)
+var a=""
+console.log(a=="")
+console.log(a==1)
+var b=[]
+var c=[]
+console.log(JSON.stringify(b)==JSON.stringify(c))
+console.log(b==c)
+
+var f={}
+var g={}
+console.log(JSON.stringify(f)==JSON.stringify(g))
+
+
